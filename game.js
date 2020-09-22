fc1a0256e6ddbbf116983b2f7fee85c031e31132
improver-lever1 <68423605+neha-cell@users.noreply.github.com>
2020-09-22 22:17:38 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ var started = false;
 var level = 0;
 
 //1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
-$(document).keydown(function() {
+$(document).click(function() {
   if (!started) {
 
     //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
@@ -26,8 +26,11 @@ $(".btn").click(function() {
   userClickedPattern.push(userChosenColour);
   console.log(userClickedPattern);
   playSound(userChosenColour);
-  animatePress(userChosenColour);
-  checkAnswer(userClickedPattern.length-1);
+
+  if(started==true){
+      animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+  }
 });
 
 function nextSequence() {
